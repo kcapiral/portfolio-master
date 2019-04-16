2331392ec4cfb05cda3226616efa2f2af1e1d4e3
Kathleen <kathleen.capiral@gmail.com>
2019-04-16 19:47:22 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function() {
+
+  $("#matagora").hover(function(){
+    $("body").css("background-color", "#DEFFF4");
+  });
+
+  $("#playground").hover(function(){
+    $("body").css("background-color", "#FFEEE8");
+  });
+
+  $("#zetacon").hover(function(){
+    $("body").css("background-color", "#8AA2A8");
+  });
+
+  $("#saiko").hover(function(){
+    $("body").css("background-color", "#D5D5FF");
+  });
+
+  $("#datacup").hover(function(){
+    $("body").css("background-color", "#EBD59B");
+  });
+
+  $("#christianschwartz").hover(function(){
+    $("body").css("background-color", "#C4EFFF");
+  });
+
+});
